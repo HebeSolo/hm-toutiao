f6068d36c03e7f44261ca01a0a84fb1ad6041a15
小仙女 <15564323492@163.com>
2019-08-22 17:11:09 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// main.js是入口文件,职责:
+// 1.所有的全局依赖包 文件 导入
+// 2.创建vue根实例
+// App.vue根组件
+
 new Vue({
   render: h => h(App)
 }).$mount('#app')
